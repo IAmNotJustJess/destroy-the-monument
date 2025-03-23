6b5e5bc8138ec3e191c5e243350104ca18c1f49d
Jessie <banderegewigz@gmail.com>
2025-03-23 01:04:25 +0100

    First commit of stuff

---
+++ src/main/java/IAmNotJustJess/destroyTheMonument/DestroyTheMonument.java
@@ -0,0 +1,17 @@
+package IAmNotJustJess.destroyTheMonument;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DestroyTheMonument extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
